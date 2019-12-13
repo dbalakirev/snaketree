fd361f11fa80f9f34bfe1dc63cc3d8e3803d0728
David Balakirev <dave00ster@gmail.com>
2019-12-13 13:36:20 +0100

    Adds bold font for properly aligned directory names

--- tree.py
+++ tree.py
@@ -5,25 +5,29 @@ import os
 # 2. print to a certain level
 # 3. print the give path
 
+def bold(text):
+    return '\033[1m' + text + '\033[0m'
+
 def print_dir_name(dir_name, indent_level):
-    print(' ', dir_name.expandtabs(indent_level), indent_level)
+    divider = '=='
+    print(divider * indent_level, dir_name, indent_level)
 
 def print_dir(dir_name, dir_path, indent_level):
     # print("debug: ", dir_name, " ", dir_path)
-    print_dir_name(dir_name, indent_level)
+    print_dir_name(bold(dir_name), indent_level)
     paths = os.listdir(dir_path)
     # print("debug: ", paths)
     for path in paths:
         # print("debug: ", path)
         if os.path.isdir(path):
-            print_dir(path+"_d", path, indent_level + 1)
+            print_dir(path, path, indent_level + 1)
         else:
             # TODO: os.path.isfile would be better to filter out devices
             print_dir_name(path, indent_level + 1)
 
 def main():
     source_path = '.'
-    indent_level = 1
+    indent_level = 0
     print_dir(source_path, source_path, indent_level)
 
 main()
