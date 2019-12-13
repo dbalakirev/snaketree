18e5c0c7d86bb432f3d56352febb96e2708a0f43
David Balakirev <dave00ster@gmail.com>
2019-12-13 13:14:25 +0100

    Printing is split to functions

--- tree.py
+++ tree.py
@@ -5,12 +5,21 @@ import os
 # 2. print to a certain level
 # 3. print the give path
 
-source_path = '.'
+def print_dir_name(dir_name, indent_level):
+    print(' ', dir_name.expandtabs(indent_level), indent_level)
 
-files = os.listdir(source_path)
-for path in files:
-    if os.path.isdir(path):
-        print("dir: ", path)
-    elif os.path.isfile(path):
-        print("file: ", path)
+def print_dir(dir_name, dir_path, indent_level):
+    print_dir_name(dir_name, indent_level)
+    paths = os.listdir(dir_path)
+    for path in paths:
+        if os.path.isdir(path):
+            print_dir(path, path, indent_level + 1)
+        elif os.path.isfile(path):
+            print_dir_name(path, indent_level + 1)
 
+def main():
+    source_path = '.'
+    indent_level = 1
+    print_dir(source_path, source_path, indent_level)
+
+main()
