c4d73a918edc8d6846a3b3ca63c82ab0452283db
David Balakirev <dave00ster@gmail.com>
2019-12-13 13:27:34 +0100

    Properly processes files in subfolders as well

--- tree.py
+++ tree.py
@@ -9,12 +9,16 @@ def print_dir_name(dir_name, indent_level):
     print(' ', dir_name.expandtabs(indent_level), indent_level)
 
 def print_dir(dir_name, dir_path, indent_level):
+    # print("debug: ", dir_name, " ", dir_path)
     print_dir_name(dir_name, indent_level)
     paths = os.listdir(dir_path)
+    # print("debug: ", paths)
     for path in paths:
+        # print("debug: ", path)
         if os.path.isdir(path):
-            print_dir(path, path, indent_level + 1)
-        elif os.path.isfile(path):
+            print_dir(path+"_d", path, indent_level + 1)
+        else:
+            # TODO: os.path.isfile would be better to filter out devices
             print_dir_name(path, indent_level + 1)
 
 def main():
