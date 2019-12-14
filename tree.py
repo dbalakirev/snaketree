16c65bafd86b720a54a06513dbd2a1ab87a6587d
David Balakirev <dave00ster@gmail.com>
2019-12-14 20:15:03 +0100

    Renamed functions

--- tree.py
+++ tree.py
@@ -9,22 +9,22 @@ import argparse
 def bold(text):
     return '\033[1m' + text + '\033[0m'
 
-def print_dir_name(dir_name, indent_level):
+def print_file_name(dir_name, indent_level):
     divider = '-'
     print(divider * indent_level, dir_name, indent_level)
 
-def print_dir(dir_name, dir_path, indent_level, maximum_depth):
+def process_files(dir_name, dir_path, indent_level, maximum_depth):
     if indent_level >= maximum_depth:
         return
-    print_dir_name(bold(dir_name), indent_level)
+    print_file_name(bold(dir_name), indent_level)
     paths = os.listdir(dir_path)
     for path in paths:
         real_path = dir_path + '/' + path
         if os.path.isdir(real_path):
-            print_dir(path, real_path, indent_level + 1, maximum_depth)
+            process_files(path, real_path, indent_level + 1, maximum_depth)
         else:
             # TODO: os.path.isfile would be better to filter out devices
-            print_dir_name(path, indent_level + 1)
+            print_file_name(path, indent_level + 1)
 
 def main():
     parser = argparse.ArgumentParser(description='Poor man\'s python implementation of the tree command')
@@ -34,6 +34,6 @@ def main():
     source_path = '.'
     maximum_depth = args.L
     depth = 0
-    print_dir(source_path, source_path, depth, maximum_depth)
+    process_files(source_path, source_path, depth, maximum_depth)
 
 main()
