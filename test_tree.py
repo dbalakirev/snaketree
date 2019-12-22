d121339a807709246ee64913edc9bb8b83106a5b
David Balakirev <dave00ster@gmail.com>
2019-12-22 12:16:46 +0100

    Counts sub folders and files

--- test_tree.py
+++ test_tree.py
@@ -1,5 +1,6 @@
 import unittest
 import tree
+import tempfile
 
 class TestTreeModule(unittest.TestCase):
 
@@ -17,6 +18,18 @@ class TestTreeModule(unittest.TestCase):
     def test_process_files_respects_depth(self):
         expected = (0,0)
         result = tree.process_files("","", indent_level = 2, maximum_depth = 2)
+        self.assertEqual(expected, result)
+
+    def test_process_files(self):
+        parent = tempfile.mkdtemp()
+        sub1 = tempfile.mkdtemp(dir=parent)
+        sub2 = tempfile.mkdtemp(dir=parent)
+        lowsub1 = tempfile.mkdtemp(dir=sub2)
+        tempfile.mkstemp(dir=sub1)
+        tempfile.mkstemp(dir=sub2)
+        tempfile.mkstemp(dir=lowsub1)
+        result = tree.process_files(parent, parent, 0, 3)
+        expected = (3,3)
         self.assertEqual(expected, result)
 
 if __name__ == '__main__':
--- tree.py
+++ tree.py
@@ -9,24 +9,26 @@ def format_path_entry(path_name, indent_level):
 
 def process_files(dir_name, dir_path, indent_level, maximum_depth):
     if indent_level >= maximum_depth:
-        return (0,0)
- 
+        return (0, 0)
+
+    # We found 'ourself'
+    found_directories = 1
+    found_files = 0
+
     dir_path_entry = format_path_entry(bold(dir_name), indent_level)
     print(dir_path_entry)
-
-    found_files = 0
-    found_directories = 0
 
     paths = os.listdir(dir_path)
     for path in paths:
         real_path = dir_path + '/' + path
         if os.path.isdir(real_path):
-            found_directories += 1
-            process_files(path, real_path, indent_level + 1, maximum_depth)
+            #found_directories += 1
+            sub_results = process_files(path, real_path, indent_level + 1, maximum_depth)
+            found_directories += sub_results[0]
+            found_files += sub_results[1]
         else:
             found_files += 1
             # TODO: os.path.isfile would be better to filter out devices
             file_path_entry = format_path_entry(path, indent_level + 1)
             print(file_path_entry)
-    
     return (found_directories, found_files)
