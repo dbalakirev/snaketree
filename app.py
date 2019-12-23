72962f14703fce9d01294c3a94e41551fab138a5
David Balakirev <dave00ster@gmail.com>
2019-12-23 19:49:45 +0100

    Adds the option to specify the directory for analysis

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import tree
 
 def main():
     parser = argparse.ArgumentParser(description='Poor man\'s python implementation of the tree command')
-    parser.add_argument("--L", default=3, type=int, help="Defines the maximum number of levels tree should go down in the hierarchy. '1' means just the direct contents of the folder.")
+    parser.add_argument("-d", "--directory", default='.', type=str, help="The directory to discover")
+    parser.add_argument("-L", "--maximum_level", default=3, type=int, help="Defines the maximum number of levels tree should go down in the hierarchy. '1' means just the direct contents of the folder.")
     args = parser.parse_args()
 
-    source_path = '.'
-    maximum_depth = args.L
+    source_path = args.directory
+    maximum_depth = args.maximum_level
     depth = 0
     found_paths = tree.process_files(source_path, source_path, depth, maximum_depth)
     print("{} directories, {} files".format(found_paths[0], found_paths[1]))
