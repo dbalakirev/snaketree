a94906a6a83949ebaff86e5f73c76a5304114528
David Balakirev <dave00ster@gmail.com>
2020-02-01 10:34:30 +0100

    Pipes for dirs

--- tree_parser.py
+++ tree_parser.py
@@ -1,13 +1,19 @@
-def tree_to_string(tree, root=True):
+def tree_to_string(tree, level=0):
     result = []
-    if root:
+    if level == 0:
         result.append(".")
+    length = len(tree.keys())
+    i = 0
     for key in tree.keys():
-        result.append("\n\u251c")
-        result.append(key)
-        # If directory
+        if i == length - 1:
+            result.append("\n\u2514")
+        else:
+            result.append("\n\u251c")
+        dash = "\u2500"
+        result.append(f'{"":{dash}<{level}} {key}')
         if type(tree[key]) is dict:
-            sub_tree = tree_to_string(tree[key], False)
+            sub_tree = tree_to_string(tree[key], level=level+1)
             result.append(sub_tree)
+        i += 1
     return "".join(result)
     #"." + "\n" + "├── .vscode" + "\n" + "│   └── file.txt" + "\n" + "└── settings.json"
